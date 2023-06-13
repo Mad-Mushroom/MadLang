7020b0c584b8f67dc6803d97ab82535c18e8a612
Mad-Mushroom <a.no.budy1@gmail.com>
2023-06-13 07:17:57 +0200

    started with lexer function

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,13 @@ config MainConfig;
 
 int main(int argc, char* argv[]){
     parseArgs(argc-1, argv);
+
+    if(MainConfig.inFile.length() <= 0 || MainConfig.outFile.length() <= 0){
+        cout << "Input/Output file not specified! Aborting." << endl;
+        return 1;
+    }
+
+    lexer();
+
     return 0;
 }
